86892dc43dc4954524993f5cfeeafe3c6bc2d012
ddoshi2021 <ddoshi2021@gmail.com>
2021-03-14 14:08:43 -0700

    Implements encoder

--- src/main/java/com/example/SpringBootSecurityDemo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/SpringBootSecurityDemo/security/ApplicationSecurityConfig.java
@@ -1,13 +1,28 @@
 package com.example.SpringBootSecurityDemo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -21,4 +36,17 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic();
     }
 
+    @Override
+    @Bean
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        UserDetails anaWoodUser = User.builder()
+                .username("anawood")
+                .password(passwordEncoder.encode("password"))
+                .roles("STUDENT")
+                .build();
+
+        return  new InMemoryUserDetailsManager(
+                anaWoodUser
+        );
+    }
 }
