2085880452d73e9563d7b50ec746969ec5536109
ddoshi2021 <ddoshi2021@gmail.com>
2021-03-13 18:06:05 -0700

    Install Spring Boot Security

---
+++ src/main/java/com/example/SpringBootSecurityDemo/security/ApplicationSecurityConfig.java
@@ -0,0 +1,12 @@
+package com.example.SpringBootSecurityDemo.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
